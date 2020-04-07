513a7968da1df084062a741b72aa7c588b3937df
agnesarth <agnesarth@hotmail.fr>
2020-04-07 12:32:46 +0200

    Ajout exo_03

---
+++ exo_03.rb
@@ -0,0 +1,10 @@
+puts hello = "Bonjour, monde !"
+#print "Et avec une voix sexy, ça donne : " 
+print hello
+
+
+# Les commentaires en Ruby commencent par un signe dièse (+ # +) et continuent jusqu’à la fin de la ligne, comme ceci:
+
+# This is a comment in Ruby
+# Bien que cela ne soit pas nécessaire, vous devez insérer un espace après le signe dièse pour améliorer la lisibilité du commentaire.
+# Lorsque vous exécutez un programme, vous ne voyez aucune indication de commentaire dans le code; l’interprète Ruby les ignore complètement
