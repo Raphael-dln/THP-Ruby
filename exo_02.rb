991d1d0d41f66b5db43d953d9579ac7a8713e7a8
agnesarth <agnesarth@hotmail.fr>
2020-04-07 12:32:13 +0200

    Ajout commentaire explication différence puts et prints

--- exo_02.rb
+++ exo_02.rb
@@ -1,3 +1,5 @@
 puts hello = "Bonjour, monde !"
 print "Et avec une voix sexy, ça donne : " 
 print hello
+
+#La commande "Puts" renvoie automatiquement à la ligne, tandis que la commande "Print" écrit sur la même ligne que la précédente commande.
